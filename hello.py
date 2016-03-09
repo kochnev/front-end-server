a89bf775cdf76b59c654092193620b1355b8e1fd
pposad <lyotchik1@mail.ru>
2016-03-09 20:52:16 +0300

    final

---
+++ hello.py
@@ -0,0 +1,8 @@
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [
+       ('Content-Type', 'text/plain')
+    ]
+    body = environ['QUERY_STRING'].replace("&","\n")
+    start_response(status, headers)
+    return [ body ]
