c9494f05411453c8fbd3f2ea35d0c53bbdbeca50
pposad <lyotchik1@mail.ru>
2016-03-10 23:20:26 +0300

    gunicorn config added

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf ~/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
+sudo ln -sf ~/web/etc/h.py   /etc/gunicorn.d/h.py
 sudo /etc/init.d/gunicorn restart
