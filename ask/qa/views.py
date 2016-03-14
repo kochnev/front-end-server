759e5e59a56a6651e9efcd4cb5bd2c4a3fe65a47
pposad <lyotchik1@mail.ru>
2016-03-14 10:18:22 +0300

    just one of the commit

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import render
+
 # Create your views here.
 from django.http import HttpResponse
 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+def test(request, *args, **kwargs):   
+    return render(request, 'index.html')    
+#return HttpResponse('OK')
--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf ~/web/etc/gunicorn.config   /etc/gunicorn.d/test
 sudo ln -sf ~/web/etc/django.config    /etc/gunicorn.d/django
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql restart
