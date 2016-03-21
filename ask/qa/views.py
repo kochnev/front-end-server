3164de4d6093b233032b086e37f4d8122b93a076
pposad <lyotchik1@mail.ru>
2016-03-21 20:10:57 +0300

    adsf

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -13,6 +13,14 @@ def register(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             new_user = form.save()
+            username = request.POST['username']
+            password = request.POST['password']
+
+             
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                 login(request, user)
+
             return HttpResponseRedirect("/")
     else:
         form = SignUpForm()
